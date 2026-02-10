9dabeebf1f92afb919cb4bc8546d85ea788727af
Titi Wangsa Damhore <111611+twdamhore@users.noreply.github.com>
2026-02-10 15:43:55 -0800

    :wqRevert "kubebuilder create api"
    This reverts commit f5a53dedbeb734e804d67f710c369bf313fec479.

--- cmd/main.go
+++ cmd/main.go
@@ -34,9 +34,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
-
-	bootv1alpha1 "github.com/isoboot/isoboot/api/v1alpha1"
-	"github.com/isoboot/isoboot/internal/controller"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -48,7 +45,6 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
-	utilruntime.Must(bootv1alpha1.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -178,13 +174,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err := (&controller.NetworkBootReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "NetworkBoot")
-		os.Exit(1)
-	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
