4cf9328ae21d25328477c0465e75e8609eddc031
Titi Wangsa Damhore <111611+twdamhore@users.noreply.github.com>
2026-03-07 16:59:36 -0800

    kubebuilder create api BootConfig

--- cmd/main.go
+++ cmd/main.go
@@ -185,6 +185,13 @@ func main() {
 		setupLog.Error(err, "Failed to create controller", "controller", "BootArtifact")
 		os.Exit(1)
 	}
+	if err := (&controller.BootConfigReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "Failed to create controller", "controller", "BootConfig")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
